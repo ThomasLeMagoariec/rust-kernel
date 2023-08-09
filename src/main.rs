fd82ae9339bed8943630beba18c36b0d3a7e5509
Thomas Le Magoariec <123191003+ThomasLeMago@users.noreply.github.com>
2023-08-09 22:47:23 +0200

    added printing through a table (test)

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod testing_stuff;
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point, bootinfo};
-use x86_64::{VirtAddr, structures::paging::PageTable};
+use x86_64::{VirtAddr, structures::paging::{PageTable, Page}};
 
 entry_point!(kernel_main);
 
@@ -22,20 +22,15 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     x708a::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    
 
     #[cfg(test)]
     test_main();
