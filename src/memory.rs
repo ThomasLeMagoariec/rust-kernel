fb17527969312936dc4382243bb4039e042803c6
Thomas Le Magoariec <123191003+ThomasLeMago@users.noreply.github.com>
2023-08-09 22:40:48 +0200

    added an empty frame allocator (test)

--- src/memory.rs
+++ src/memory.rs
@@ -1,8 +1,29 @@
-use x86_64::{VirtAddr, structures::paging::{PageTable, OffsetPageTable}, PhysAddr};
+use x86_64::{VirtAddr, structures::paging::{PageTable, OffsetPageTable, FrameAllocator, PhysFrame, Mapper, Page, Size4KiB}, PhysAddr};
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        None
+    }
+}
+
+pub fn create_example_mapping(page: Page, mapper: &mut OffsetPageTable, frame_allocator: &mut impl FrameAllocator<Size4KiB>) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+
+    map_to_result.expect("map_to failed").flush();
 }
 
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
