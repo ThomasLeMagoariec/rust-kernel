e3db64b6f6ea3a05e51a0d977a8c6c38b2bf39b0
Thomas Le Magoariec <123191003+ThomasLeMago@users.noreply.github.com>
2023-08-09 10:02:26 +0200

    basic rust kernel has support for very few things

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(x708a::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+mod serial;
+mod testing_stuff;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    x708a::init();
+
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
+    #[cfg(test)]
+    test_main();
+
+    println!("it didn't crash!");
+
+    x708a::hlt_loop();
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    x708a::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    x708a::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1)
+}
---
+++ src/testing_stuff.rs
@@ -0,0 +1,6 @@
+use crate::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
