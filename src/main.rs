c5d21c7736e8ace2bd8b4af4ad588f351f87e6f9
Thomas Le Magoariec <123191003+ThomasLeMago@users.noreply.github.com>
2023-08-09 16:32:45 +0200

    on start now prints entries in level 4 & 3 tables

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,37 @@ mod serial;
 mod testing_stuff;
 
 use core::panic::PanicInfo;
-use bootloader::{BootInfo, entry_point};
+use bootloader::{BootInfo, entry_point, bootinfo};
+use x708a::memory::active_level_4_table;
+use x86_64::{VirtAddr, structures::paging::PageTable};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello World{}", "!");
-
-    x708a::init();
-
     use x86_64::registers::control::Cr3;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    println!("Hello World{}", "!");
+    x708a::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 entry {}: {:?}", i, entry);
+                }
+            }
+        }
+    }
 
     #[cfg(test)]
     test_main();
